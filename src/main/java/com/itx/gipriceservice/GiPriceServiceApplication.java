4484c62eb9a88509420e277ca1c0cfbfee344b63
Jonatan Fernandez Frias <programadorjff@gmail.com>
2026-04-15 22:03:34 +0200

    chore: bootstrap Spring Boot reactive project with WebFlux, R2DBC, MapStruct and OpenAPI generator

---
+++ src/main/java/com/itx/gipriceservice/GiPriceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.itx.gipriceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiPriceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiPriceServiceApplication.class, args);
+    }
+
+}
