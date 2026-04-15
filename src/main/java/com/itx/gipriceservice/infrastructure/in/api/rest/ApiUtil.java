96e127871f4d86d315f7c5cb21000b9988a38c6c
Jonatan Fernandez Frias <programadorjff@gmail.com>
2026-04-15 22:47:55 +0200

    feat: define OpenAPI contract and generate controller interface and response model

---
+++ src/main/java/com/itx/gipriceservice/infrastructure/in/api/rest/ApiUtil.java
@@ -0,0 +1,23 @@
+package com.itx.gipriceservice.infrastructure.in.api.rest;
+
+import java.nio.charset.StandardCharsets;
+import org.springframework.core.io.buffer.DefaultDataBuffer;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public final class ApiUtil {
+
+    private ApiUtil() {}
+
+    public static Mono<Void> getExampleResponse(ServerWebExchange exchange, MediaType mediaType, String example) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.getHeaders().setContentType(mediaType);
+
+        byte[] exampleBytes = example.getBytes(StandardCharsets.UTF_8);
+        DefaultDataBuffer data = new DefaultDataBufferFactory().wrap(exampleBytes);
+        return response.writeWith(Mono.just(data));
+    }
+}
